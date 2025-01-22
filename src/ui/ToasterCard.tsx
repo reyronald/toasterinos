e9d9229d580f2f86b6aa6a77e55d3cdce9028d75
Ronald Rey <reyronald@gmail.com>
2025-01-22 19:34:39 -0400

    slideun keyframe animation for card

--- src/ui/ToasterCard.tsx
+++ src/ui/ToasterCard.tsx
@@ -2,7 +2,23 @@ import { PropsWithChildren } from "react";
 
 export function ToasterCard(props: PropsWithChildren) {
   return (
-    <div className="bg-black shadow-md rounded-lg px-8 py-4 text-3xl border-2 border-slate-400 flex justify-around gap-8 items-center">
+    <div
+      className={[
+        "bg-black",
+        "shadow-md",
+        "rounded-lg",
+        "px-8",
+        "py-4",
+        "text-3xl",
+        "border-2",
+        "border-slate-400",
+        "flex",
+        "justify-around",
+        "gap-8",
+        "items-center",
+        "animate-slideup",
+      ].join(" ")}
+    >
       {props.children}
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideup: {
+          from: { transform: "translateY(100%)" },
+          to: { transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        slideup: "slideup 0.2s cubic-bezier(0.25, 0.8, 0.25, 1)",
+      },
+    },
   },
   plugins: [],
 };
